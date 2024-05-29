4eedde8ef1700f86c48f0949601bea4b945f30f8
sa11748 <shubhangishubhi490@gmail.com>
2024-05-29 19:58:52 +0530

    Some changes

--- src/pages/Home/HeroSection.jsx
+++ src/pages/Home/HeroSection.jsx
@@ -3,10 +3,10 @@ export default function HeroSection() {
       <section id="heroSection" className="hero--section">
         <div className="hero--section--content--box">
           <div className="hero--section--content">
-            <p className="section--title">Hey, I'm John</p>
+            <p className="section--title">Hey, I'm Shubhangi</p>
             <h1 className="hero--section--title">
               <span className="hero--section-title--color">Full Stack</span>{" "}
-              <br />
+              {/* <br /> */}
               Developer
             </h1>
             <p className="hero--section-description">
@@ -17,7 +17,7 @@ export default function HeroSection() {
           <button className="btn btn-primary">Get In Touch</button>
         </div>
         <div className="hero--section--img">
-          <img src="./img/my.jpg" alt="Hero Section" />
+          <img src="./img/my.png" alt="Hero Section" />
         </div>
       </section>
     );
